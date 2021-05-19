f80681565fe439e2b5bd28b3b07da752f6a225f8
zhuliminl <zhuliminl@gmail.com>
2021-05-19 20:03:21 +0800

    xxxx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import logo from './logo.svg';
 import './App.css';
+import { useCallback, useEffect, useState } from 'react'
 
 function App() {
+  console.log('saul >>>>>> 网页 APP 打印', window.chrome, window)
+  const [userText, setUserText] = useState('');
+  const [textSelected, setSelectedText] = useState('')
+
+  const handleUserKeyPress = useCallback(event => {
+    const { key, keyCode } = event;
+
+    if (keyCode === 32 || (keyCode >= 65 && keyCode <= 90)) {
+      setUserText(prevUserText => `${prevUserText}${key}`);
+    }
+  }, []);
+
+  const messagesFromReactAppListener = (data, data1) => {
+    console.log('saul DDDDDDDDDDDDDDDDDDDDDD', data, data1)
+  }
+
+  useEffect(() => {
+    // window.addEventListener('keydown', handleUserKeyPress);
+
+
+    document.ondblclick = function () {
+      const _document = document
+      const selection = _document.selection
+      var sel = (selection && selection.createRange().text) ||
+        (window.getSelection && window.getSelection().toString());
+
+      setSelectedText(sel)
+
+      // alert(sel);
+      window.chrome.runtime.sendMessage({
+        action: 'send_word',
+        content: sel,
+      }, res => {
+        console.log('res jjjjjjj ======', res)
+      })
+    };
+
+
+
+    return () => {
+      // window.removeEventListener('keydown', handleUserKeyPress);
+    };
+  }, [handleUserKeyPress]);
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>{textSelected}</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -15,7 +63,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          {userText}
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -8,10 +8,12 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('like_button_container')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// console.log('saul >>>>>>>来自网页 app拍')
