014d0b6769255cf0e4ac8abae823a859c87585c3
zhuliminl <zhuliminl@gmail.com>
2021-05-20 11:42:29 +0800

    xxxx

--- src/App.js
+++ src/App.js
@@ -51,21 +51,9 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <h1>xxxx</h1>
         <h1>{textSelected}</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           {userText}
-        </a>
-      </header>
     </div>
   );
 }
