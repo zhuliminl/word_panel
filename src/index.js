47b629809ac1b2f84821f2fbd31714d5d7d96a31
zhuliminl <zhuliminl@gmail.com>
2021-05-20 10:33:23 +0800

    xxxx

--- src/index.js
+++ src/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('like_button_container')
+  document.getElementById('word_app')
 );
